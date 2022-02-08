cf2e9b70e33c848c86a459d71558790b21833543
naman0058 <jnaman345@gmail.com>
2022-02-08 14:39:33 +0530

    a

--- routes/Vendor/merchant_api.js
+++ routes/Vendor/merchant_api.js
@@ -4,6 +4,7 @@ var pool =  require('../pool');
 var upload = require('../multer');
 
 
+// Add Product Start
 
 router.get('/add-product',(req,res)=>{
      res.render('Merchant_Webview/product',{vendorid:req.query.vendorid,categoryid:req.query.categoryid});
@@ -132,5 +133,37 @@ router.get('/delete',(req,res)=>{
     })
 })
 
+
+
+// Add Products End
+
+
+// Coupon Start
+
+router.get('/add-coupon',(req,res)=>{
+    res.render('Merchant_Webview/coupon',{vendorid:req.query.vendorid});
+})
+
+
+router.post('/coupon/insert',upload.single('image'),(req,res)=>{
+    let body = req.body
+   body['image'] = req.file.filename;
+  console.log('mydata',req.body);
+   pool.query(`insert into coupon set ?`,body,(err,result)=>{
+       err ? console.log(err) : res.json({msg : 'success'})
+   })
+})
+
+router.get('/coupon-show',(req,res)=>{
+    pool.query(`select p.* from coupon p where p.vendorid = ${req.query.vendorid}`,(err,result)=>{
+        err ? console.log(err) : res.json(result)
+    })
+})
+
+router.get('/coupon/delete',(req,res)=>{
+    pool.query(`delete from coupon where id = '${req.query.id}'`,(err,result)=>{
+        err ? console.log(err) : res.json(result)
+    })
+})
 
 module.exports = router;

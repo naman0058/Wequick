ca4c75a347b3e35c2f91bbb871ed0b3c3b7b1d71
naman0058 <jnaman345@gmail.com>
2022-02-22 00:45:36 +0530

    a

--- routes/Agent/api.js
+++ routes/Agent/api.js
@@ -9,7 +9,7 @@ router.post('/validate',(req,res)=>{
     pool.query(`select * from agent where number = '${req.body.number}'`,(err,result)=>{
         if(err) throw err;
         else if(result[0]){
-            res.json({msg:success,result})
+            res.json({msg:'success',result})
         }
         else {
             res.json({msg:'invalid'})

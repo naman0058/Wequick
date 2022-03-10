98a53f9f45a6c1fa65b5e4bd65ce8c9a10bc01fe
naman0058 <jnaman345@gmail.com>
2022-03-10 15:01:12 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -3,11 +3,11 @@ var mysql = require('mysql')
 
 const pool = mysql.createPool({
  
-   host : '167.71.231.201',
-    user: 'root',
+   host : 'db-mysql-blr1-27224-do-user-11060553-0.b.db.ondigitalocean.com',
+    user: 'doadmin',
     password : '123a@8Anmanraspaa123a@*Anmanraspaa',
-    database: 'wequick',
-    port:'3306',
+    database: 'nR4wZPTOvPY3Mep8',
+    port:'25060',
     multipleStatements: true
   })
 

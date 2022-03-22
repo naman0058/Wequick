6731de5ce1eebb28a0d07ef1c48b676bf2828dd2
naman0058 <42539761+naman0058@users.noreply.github.com>
2022-03-22 19:23:38 +0530

    Direct Updates

--- routes/api.js
+++ routes/api.js
@@ -195,13 +195,6 @@ router.get('/get-category',(req,res)=>{
    })
 
 
-   router.get('/get-city',(req,res)=>{
-    pool.query(`select c.* , (select s.name from state s where s.id = c.stateid) as state_name from city c order by id desc`,(err,result)=>{
-        if(err) throw err;
-        else res.json(result)
-    })
-   
-   })
 
 
 

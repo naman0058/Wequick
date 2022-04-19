0330a19031eeef4de637e7929466c9204e002b26
naman0058 <jnaman345@gmail.com>
2022-04-19 13:01:51 +0530

    a

--- routes/api.js
+++ routes/api.js
@@ -2174,7 +2174,7 @@ pool.query(`update vendor set ? where number = ?`, [req.body, req.body.number], 
 
 
 router.get('/check-update',(req,res)=>{
-  if(req.query.version == 3){
+  if(req.query.version == 5){
     res.json({msg:'updated'})
 
   }

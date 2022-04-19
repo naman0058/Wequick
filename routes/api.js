3bec5bb72824684878a1f2f70b3d00c54a4fb04f
naman0058 <jnaman345@gmail.com>
2022-04-19 13:18:39 +0530

    a

--- routes/api.js
+++ routes/api.js
@@ -2536,9 +2536,7 @@ router.get('/get-rating',(req,res)=>{
 
 router.get('/single-vendor-details',(req,res)=>{
 
-
   var today = new Date();
-
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();

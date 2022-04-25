7e9a9f212a041d5a56bc47f8633492903b09bbad
naman0058 <jnaman345@gmail.com>
2022-04-25 13:10:36 +0530

    a

--- routes/HospitalManagement/api.js
+++ routes/HospitalManagement/api.js
@@ -9,31 +9,31 @@ let table = 'hospital_management'
 router.post('/add-staff',upload.fields([{ name: 'image', maxCount: 1 }, { name: 'aadhar_card_front', maxCount: 8 } , { name: 'aadhar_card_back', maxCount: 8 } , { name: 'pan_card', maxCount: 8 } , , { name: 'higher_education_marksheet', maxCount: 8 }]),(req,res)=>{
     let body = req.body
  
-    console.log(req.files)
+    // console.log(req.files)
     
 
 
-    body['image'] = req.files.image[0].filename;
-    body['aadhar_card_front'] = req.files.aadhar_card_front[0].filename;
-    body['aadhar_card_back'] = req.files.aadhar_card_back[0].filename;
-    body['pan_card'] = req.files.pan_card[0].filename;
-    body['higher_education_marksheet'] = req.files.higher_education_marksheet[0].filename;
+    // body['image'] = req.files.image[0].filename;
+    // body['aadhar_card_front'] = req.files.aadhar_card_front[0].filename;
+    // body['aadhar_card_back'] = req.files.aadhar_card_back[0].filename;
+    // body['pan_card'] = req.files.pan_card[0].filename;
+    // body['higher_education_marksheet'] = req.files.higher_education_marksheet[0].filename;
 
 
 
 
-    var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    // var today = new Date();
+    // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+    // var dd = String(today.getDate()).padStart(2, '0');
+    // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    // var yyyy = today.getFullYear();
     
-    today = yyyy + '-' + mm + '-' + dd;
+    // today = yyyy + '-' + mm + '-' + dd;
     
     
-      body['date'] = today;
-      body['time'] = time;
+    //   body['date'] = today;
+    //   body['time'] = time;
 
  console.log(req.body)
  res.json(req.body)

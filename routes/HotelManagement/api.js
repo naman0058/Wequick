dba4d42418dbcd8b1db1cca346497d666cf85eef
naman0058 <jnaman345@gmail.com>
2022-04-30 18:37:59 +0530

    a

--- routes/HotelManagement/api.js
+++ routes/HotelManagement/api.js
@@ -191,35 +191,86 @@ router.get('/delete-staff-designation',(req,res)=>{
 
 router.post('/add-hotel-user',upload.fields([{ name: 'aadhar_front', maxCount: 1 } , { name: 'aadhar_back', maxCount: 1 }]),(req,res)=>{
     let body = req.body
-  res.json(req.files)
+//   res.json(req.files)
 
 
-//     body['aadhar_front'] = req.files.aadhar_front[0].filename;
-//     body['aadhar_back'] = req.files.aadhar_back[0].filename;
+    body['aadhar_front'] = req.files.aadhar_front[0].filename;
+    body['aadhar_back'] = req.files.aadhar_back[0].filename;
  
   
-//     var today = new Date();
-//     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     
-//     var dd = String(today.getDate()).padStart(2, '0');
-//     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-//     var yyyy = today.getFullYear();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
     
-//     today = yyyy + '-' + mm + '-' + dd;
+    today = yyyy + '-' + mm + '-' + dd;
     
     
-//       body['date'] = today;
-//       body['time'] = time;
-//   console.log(body)
+      body['date'] = today;
+      body['time'] = time;
+  console.log(body)
 
   
-//   console.log(req.body)
-//      pool.query(`insert into hotel_user set ?`,body,(err,result)=>{
-//       if(err) res.json(err);
-//       else res.json({msg:'success'})
-//      })
+  console.log(req.body)
+     pool.query(`insert into hotel_user set ?`,body,(err,result)=>{
+      if(err) res.json(err);
+      else res.json({msg:'success'})
+     })
   })
     
+
+ 
+
+  router.post('/add-hotel-billing',(req,res)=>{
+    let body = req.body
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    today = yyyy + '-' + mm + '-' + dd;
+    
+    
+      body['date'] = today;
+      body['time'] = time;
+      body['status'] = 'pending'
+  
+  console.log(req.body)
+     pool.query(`insert into hotel_billing set ?`,body,(err,result)=>{
+      if(err) res.json(err);
+      else res.json({msg:'success'})
+     })
+  })
+  
+
+  router.get('/get-hotel-billing',(req,res)=>{
+    pool.query(`select * from hotel_billing where vendorid = '${req.query.vendorid}' and room_no = '${req.query.room_no}' and number = '${req.query.number}' and status = 'pending'`,(err,result)=>{
+        if(err) throw err;
+        else res.json(result)
+    })
+})
+
+
+
+router.get('/get-hotel-billing',(req,res)=>{
+    pool.query(`select * from hotel_billing where vendorid = '${req.query.vendorid}' and id = '${req.query.id}'`,(err,result)=>{
+        if(err) throw err;
+        else res.json(result);
+    })
+})
+
+
+router.post('/checkout',(req,res)=>{
+    let body = req.body;
+    pool.query(`update hotel_billing set status = 'complete' where number = '${req.body.number}' and room_no = '${req.body.room_no}'`,(err,result)=>{
+        if(err) throw err;
+        else res.json(result);
+    })
+});
+
 
 
 
